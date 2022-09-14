21e040bb8970e1b853ead2f7b3495a8957eeca23
Apple <apple@appurunoiMac.local>
2022-09-14 22:58:57 +0700

    Push refresh token

--- src/main/java/com/carmanager/carapp/security/SecurityConfig.java
+++ src/main/java/com/carmanager/carapp/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.carmanager.carapp.security;
 
 
 import com.carmanager.carapp.filter.CustomAuthenticationFilter;
+import com.carmanager.carapp.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -35,11 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers( "/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers( "/api/login/**","api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "api/users/**").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST, "api/users/save/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(POST, "api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class );
     }
 
     @Bean
