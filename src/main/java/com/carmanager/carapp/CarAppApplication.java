3f6220c7d8e24d8a9f26e8ba59b27d5c2e6fa7f7
Apple <apple@appurunoiMac.local>
2022-09-16 09:17:59 +0700

    complete a little bit

--- src/main/java/com/carmanager/carapp/CarAppApplication.java
+++ src/main/java/com/carmanager/carapp/CarAppApplication.java
@@ -24,28 +24,27 @@ public class CarAppApplication {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    CommandLineRunner run(UserService userService){
-        return args->{
-            userService.saveRole(new Role(null,"ROLE_USER"));
-            userService.saveRole(new Role(null,"ROLE_ADMIN"));
-            userService.saveRole(new Role(null,"ROLE_MANAGER"));
-            userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
-
-            userService.saveUser(new User(null,"Van Tran","vantran12","1234", new ArrayList<>()));
-            userService.saveUser(new User(null,"Quang Sang","sangthai","1234", new ArrayList<>()));
-            userService.saveUser(new User(null,"Xuan Nhat","nhatban","1234", new ArrayList<>()));
-            userService.saveUser(new User(null,"Thanh Nghi","heo9.5","1234", new ArrayList<>()));
-
-            userService.addRoleToUser("vantran12","ROLE_ADMIN");
-            userService.addRoleToUser("vantran12","ROLE_SUPER_ADMIN");
-            userService.addRoleToUser("sangthai","ROLE_MANAGER");
-            userService.addRoleToUser("sangthai","ROLE_ADMIN");
-            userService.addRoleToUser("nhatban","ROLE_USER");
-            userService.addRoleToUser("nhatban","ROLE_ADMIN");
-            userService.addRoleToUser("heo9.5","ROLE_USER");
-        };
-
-    }
+//    @Bean
+//    CommandLineRunner run(UserService userService){
+//        return args->{
+//            userService.saveRole(new Role(null,"USER"));
+//            userService.saveRole(new Role(null,"ADMIN"));
+//            userService.saveRole(new Role(null,"MANAGER"));
+//            userService.saveRole(new Role(null,"SUPER_ADMIN"));
+//
+//            userService.saveUser(new User(null,"Van Tran","vantran12","1234", new ArrayList<>()));
+//            userService.saveUser(new User(null,"Quang Sang","sangthai","1234", new ArrayList<>()));
+//            userService.saveUser(new User(null,"Xuan Nhat","nhatban","1234", new ArrayList<>()));
+//            userService.saveUser(new User(null,"Thanh Nghi","heo9.5","1234", new ArrayList<>()));
+//
+//            userService.addRoleToUser("vantran12","ADMIN");
+//            userService.addRoleToUser("vantran12","SUPER_ADMIN");
+//            userService.addRoleToUser("sangthai","MANAGER");
+//            userService.addRoleToUser("sangthai","ADMIN");
+//            userService.addRoleToUser("nhatban","ADMIN");
+//            userService.addRoleToUser("heo9.5","USER");
+//        };
+//
+//    }
 
 }
--- src/main/java/com/carmanager/carapp/security/SecurityConfig.java
+++ src/main/java/com/carmanager/carapp/security/SecurityConfig.java
@@ -37,9 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers( "/api/login/**","api/token/refresh/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "api/users/**").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST, "api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers( "/api/login/**","/api/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/users/**").hasAnyAuthority("USER");
+        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class );
